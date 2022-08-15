ffcd52ad86b4dcf2661726ccc81acdce24dfc8df
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-08-15 18:38:42 +0200

    Add parents property and validation for consistency

--- scripts/generate-alias-key-json.py
+++ scripts/generate-alias-key-json.py
@@ -3,6 +3,7 @@
 import json
 import yaml
 import glob
+import itertools
 
 # A is implicitly aliased to itself
 aliases = {'A': 'A'}
@@ -10,10 +11,7 @@ aliases = {'A': 'A'}
 # Iterate through all lineage definition files
 for yaml_file in glob.glob('lineages/*.yml'):
     with open(yaml_file, 'r') as stream:
-        try:
-            yaml_data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+        yaml_data = yaml.safe_load(stream)
     if 'alias' in yaml_data:
         # Alias key json maps from alias to completely unaliased
         aliases[yaml_data['alias']] = yaml_data['unaliased_name']
@@ -27,10 +25,7 @@ with open('auto-generated/alias_key.json', 'w') as outfile:
 validation_failed = False
 for yaml_file in glob.glob('lineages/*.yml'):
     with open(yaml_file, 'r') as stream:
-        try:
-            yaml_data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+        yaml_data = yaml.safe_load(stream)
     # Unalias (aliased) name
     aliased_name = yaml_data['name']
     split_name = aliased_name.split('.')
@@ -54,5 +49,33 @@ for yaml_file in glob.glob('lineages/*.yml'):
         print(f"Aliasing validation failed for {yaml_file}")
         print(f"Name contains alias {aliased_prefix} that is not defined as alias anywhere")
         validation_failed = True
+
+#%%
+# Validate parents
+validation_failed = False
+for yaml_file in glob.glob('lineages/*.yml'):
+    with open(yaml_file, 'r') as stream:
+        yaml_data = yaml.safe_load(stream)
+    parent = yaml_data['parent']
+    # Alias of (unaliased name's parent)
+    # Unaliased parent name
+    unaliased_name = yaml_data['unaliased_name']
+    unaliased_name_split = unaliased_name.split('.')
+    if len(unaliased_name_split) <= 1:
+        if parent != 'None':
+            print(f"Parent validation failed for {yaml_file}")
+            print(f"parent:{parent} != None")
+            validation_failed = True
+    else:
+        unaliased_parent_split = unaliased_name_split[0:-1]
+        unaliased_parent_from_unaliased_name = '.'.join(unaliased_parent_split)
+
+        parent_split = parent.split('.')
+        parent_unaliased_split = itertools.chain.from_iterable([aliases[parent_split[0]].split('.'), parent_split[1:]])
+        parent_unaliased = '.'.join(parent_unaliased_split)
+        if parent_unaliased != unaliased_parent_from_unaliased_name:
+            print(f"Parent validation failed for {yaml_file}")
+            print(f"parent:{parent} != deduced_parent:{unaliased_parent_from_unaliased_name}")
+            validation_failed = True
 
 exit(1 if validation_failed else 0)
